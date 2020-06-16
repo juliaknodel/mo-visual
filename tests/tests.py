efa3bd15933d7ea9827d5bac069c4963b80b04b4
juliaknodel <juliaknodel13@gmail.com>
2020-06-16 21:24:48 +0300

    Add descriptions of the functions and change tests names

--- tests/tests.py
+++ tests/tests.py
@@ -1,33 +1,43 @@
-import math
+# import math
+from math import ceil, log
 import unittest
 from sympy.abc import x
 from source.golden_section import golden_section_method, PHI1
 
 
 def get_problem_params():
-    a = -1
-    b = 1
-    eps = 0.1
-    expr = x**2
+    """
+    :return: 1. function - function to be minimized
+             2. left_border - left border of the initial uncertainty interval
+             3. right_border - right border of the initial uncertainty interval
+             4. accuracy  - required accuracy of the solution
+             5. n_exp - theoretical number of iterations
+    """
+    left_border = -1
+    right_border = 1
+    accuracy = 0.1
+    function = x**2
 
-    # n высчитывается из теории
-    n_exp = int(math.ceil(math.log(eps / (b-a)) / math.log(PHI1)))
-    return expr, a, b, eps, n_exp
+    # theoretical number of iterations
+    n_exp = int(ceil(log(accuracy / (right_border - left_border)) / log(PHI1)))
+
+    return function, left_border, right_border, accuracy, n_exp
 
 
 class TestGoldenSectionMethod(unittest.TestCase):
-    def test_GSM_GetProblem_ShouldReturnTheoryAndRealIterationsEqual(self):
+
+    def test_GSM_ChecksIfTheNumOfIterationsEqualsTheoreticalEstimation(self):
         expr, a, b, eps, n_exp = get_problem_params()
         results = golden_section_method(expr, a, b, eps)
         iterations_list, actual_iterations_num = results
         self.assertEqual(actual_iterations_num, n_exp)
 
-    def test_GSM_GetProblem_ShouldReturnEndIntervalSizeLessThanAccuracy(self):
+    def test_GSM_ChecksIfLenOfFinalUncertaintyIntervalLessThanAccuracy(self):
         expr, a, b, eps, n_exp = get_problem_params()
 
         results = golden_section_method(expr, a, b, eps)
         iterations_list, actual_iterations_num = results
 
         last_iter = iterations_list[actual_iterations_num]
-        end_interval_size = abs(last_iter[len(last_iter) - 1] - last_iter[0])
-        self.assertEqual(end_interval_size < eps, True)
+        final_interval_length = abs(last_iter[-1] - last_iter[0])
+        self.assertEqual(final_interval_length < eps, True)
